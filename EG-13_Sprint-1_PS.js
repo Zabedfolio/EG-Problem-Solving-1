6985d9528b16a36022512da4853b04631698a463
Zabedfolio <zabedfolio@gmail.com>
2026-08-21 10:44:04 +0600

    first commit

---
+++ EG-13_Sprint-1_PS.js
@@ -0,0 +1,17 @@
+function isLeapYear(year){
+   if((year%4==0 && year%100!=0)|| year%400==0){
+    return true;
+   }
+   else return false;
+}
+
+// const result = isLeapYear(2029)
+// console.log(result)
+
+
+
+
+function generateFibonacci(n){
+    // Write your code here
+}
+
