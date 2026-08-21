b7c2320a3659d1f7b64edee289be6e6b28935c20
Zabedfolio <zabedfolio@gmail.com>
2026-08-21 11:01:18 +0600

    added the second prob

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -1,8 +1,8 @@
-function isLeapYear(year){
-   if((year%4==0 && year%100!=0)|| year%400==0){
-    return true;
-   }
-   else return false;
+function isLeapYear(year) {
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        return true;
+    }
+    else return false;
 }
 
 // const result = isLeapYear(2029)
@@ -11,7 +11,23 @@ function isLeapYear(year){
 
 
 
-function generateFibonacci(n){
-    // Write your code here
+function generateFibonacci(n) {
+    let first = 0
+    let second = 1
+    const arr = [first,second]
+    for(let i=2;i<n;i++){
+        let next = first+second;
+        first = second;
+        second = next;
+        arr.push(next)
+    }
+    return arr;
 }
+// const result = generateFibonacci(7);
+// console.log(result);
+
+
+
+
+
 
