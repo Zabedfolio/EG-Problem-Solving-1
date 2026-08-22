1d5345b80486fba23cd7003409a843a0e7560463
Zabedfolio <zabedfolio@gmail.com>
2026-08-22 14:11:12 +0600

    fifth one

--- EG-13_Sprint-1_PS.js
+++ EG-13_Sprint-1_PS.js
@@ -56,6 +56,22 @@ function findLCM(a, b){
 // const result = findLCM(12,18)
 // console.log(result);
 
+function isPrime(num){
+    if(num<=1) return false;
+    if(num === 2) return true;
+    if(num%2===0) return false;
+
+    for(let i=3; i*i<=num;i+=2){
+        if(num%i==0){
+            return false;
+        }
+    }
+    return true;
+}
+// const result = isPrime(30);
+// console.log(result);
+
+
 
 
 
